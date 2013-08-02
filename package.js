18906085534e4c5f74e215e5b6d68da51d269739
Mathieu Bouchard <matb33@gmail.com>
2013-08-02 13:28:54 -0400

    inc version, clean-up unused

--- package.js
+++ package.js
@@ -15,13 +15,10 @@ Package.on_test(function (api) {
 	api.use(["coffeescript"], "server");
 	api.use(["accounts-base", "accounts-testing"], both);
 
-	// TODO: the most important tests, the ones for client -> server
-	// hooks (a client-initiated insert/update/remove that runs a server
-	// hook) are not yet written. If you can figure out how, please tell
-	// me or help me write these tests. I can't figure out how to run a
-	// Tinytest that relies on both server and client talking to each other.
 	api.add_files("tests.js", both);
 	api.add_files("tests_publish.js", both);
 	api.add_files("tests_userid_in_find_hooks_within_publish.js", both);
+
+	// TODO: @mizzao, is this still applicable?
 	//api.add_files(["client_server_userId_tests.coffee"], both);
 });
