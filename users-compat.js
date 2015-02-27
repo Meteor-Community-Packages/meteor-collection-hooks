ee0c485f28c605ff4d66aadb034da51a206b2486
Richard Lai <it.rclai@gmail.com>
2015-02-27 14:45:09 -0500

    Removed reassigning Meteor.users prototype

--- users-compat.js
+++ users-compat.js
@@ -1,7 +1,4 @@
 if (Meteor.users) {
-  // If Meteor.users has been instantiated, attempt to re-assign its prototype:
-  CollectionHooks.reassignPrototype(Meteor.users);
-
   // Next, give it the hook aspects:
   CollectionHooks.extendCollectionInstance(Meteor.users);
 }
