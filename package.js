f233f318b29d4c08a0d27d91a0b11193120c23e4
Richard Lai <it.rclai@gmail.com>
2015-02-26 22:41:13 -0500

    Upgrade collection-extensions dependency

--- package.js
+++ package.js
@@ -32,7 +32,7 @@ Package.onUse(function (api, where) {
   ]);
 
   api.use([
-    "lai:collection-extensions@0.1.1"
+    "lai:collection-extensions@0.1.3"
   ]);
 
   api.use(["accounts-base"], ["client", "server"], { weak: true });
