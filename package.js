d829bc6a9330e416afa780d1f847c966040a96e6
Jan Dvorak <storytellercz@proton.me>
2022-09-03 13:37:30 +0200

    Published matb33:collection-hooks@1.1.4

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: 'matb33:collection-hooks',
   summary: 'Extends Mongo.Collection with before/after hooks for insert/update/remove/find/findOne',
-  version: '1.1.3',
+  version: '1.1.4',
   git: 'https://github.com/Meteor-Community-Packages/meteor-collection-hooks'
 })
 
