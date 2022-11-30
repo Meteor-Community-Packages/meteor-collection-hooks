7eccc2e42a35948af8e8b2dfe6d9c88801feb121
Long Nguyen <olragon@users.noreply.github.com>
2022-11-30 11:44:30 +0700

    Update collection-hooks.d.ts

--- collection-hooks.d.ts
+++ collection-hooks.d.ts
@@ -1,13 +1,17 @@
 declare module 'meteor/matb33:collection-hooks' {
     import { Meteor } from "meteor/meteor"
+    type Options = {
+        fetchPrevious?: boolean
+        [key: string]: any
+    }
     type TGlobalOptions = {
-        all?: any
-        insert?: any
-        update?: any
-        upsert?: any
-        find?: any
-        findOne?: any
-        remove?: any
+        all?: Options
+        insert?: Options
+        update?: Options
+        upsert?: Options
+        find?: Options
+        findOne?: Options
+        remove?: Options
     }
     interface CollectionHooks {
         defaultUserId?: string
@@ -24,15 +28,32 @@ declare module 'meteor/matb33:collection-hooks' {
 declare module 'meteor/mongo' {
     import {CollectionHooks} from "meteor/matb33:collection-hooks";
     module Mongo {
-        type THookThis<UnderlyingMethod extends (...args: any) => any> = {_super: UnderlyingMethod, context: ThisType<UnderlyingMethod>, args: Parameters<UnderlyingMethod>}
-        type THookBeforeInsert<T,O = void> = (this: THookThis<Collection<T>["insert"]>, userId: string|undefined, doc: T) => O;
-        type THookBeforeUpdate<T,O = void> = (this: THookThis<Collection<T>["update"]>, userId: string|undefined, doc: T, fieldNames: string[], modifier: any, options: any) => O
-        type THookBeforeRemove<T,O = void> = (this: THookThis<Collection<T>["remove"]>, userId: string|undefined, doc: T) => O
-        type THookBeforeUpsert<T,O = void> = (this: THookThis<Collection<T>["upsert"]>, userId: string|undefined, selector: any, modifier: any, options: any) => O
-        type THookBeforeFind<T,O = void> = (this: THookThis<Collection<T>["find"]>, userId: string|undefined, selector: any, options: any) => O
-        type THookAfterFind<T> = (this: THookThis<Collection<T>["find"]>, userId: string|undefined, selector: any, options: any, cursor: Cursor<T>) => void
-        type THookBeforeFindOne<T,O = void> = (this: THookThis<Collection<T>["findOne"]>, userId: string|undefined, selector: any, options: any) => O
-        type THookAfterFindOne<T> = (this: THookThis<Collection<T>["findOne"]>, userId: string|undefined, selector: any, options: any, doc: T) => void
+        type GenericFunction = (...args: any) => any
+        type THookThis<T, UnderlyingMethod extends GenericFunction> = {
+            _super: UnderlyingMethod,
+            context: ThisType<UnderlyingMethod>,
+            args: Parameters<UnderlyingMethod>
+            transform: (doc: T) => T
+        }
+        type THookThisWithId<T, UnderlyingMethod extends GenericFunction> = THookThis<T, UnderlyingMethod> & {
+            _id: string
+        }
+        type THookThisWithTransform<T, UnderlyingMethod extends GenericFunction> = THookThis<T, UnderlyingMethod> & {
+            transform: (doc: T) => T
+        }
+        type THookThisWithTransformAndPrevious<T, UnderlyingMethod extends GenericFunction> = THookThisWithTransform<T, UnderlyingMethod> & {
+            previous: T
+        }
+        type THookBeforeInsert<T,O = void> = (this: THookThis<T, Collection<T>["insert"]>, userId: string|undefined, doc: T) => O;
+        type THookAfterInsert<T,O = void> = (this: THookThisWithId<T, Collection<T>["insert"]>, userId: string|undefined, doc: T) => O;
+        type THookBeforeUpdate<T,O = void> = (this: THookThis<T, Collection<T>["update"]> & { previous: T, transform: (doc: T) => T }, userId: string|undefined, doc: T, fieldNames: string[], modifier: any, options: any) => O
+        type THookAfterUpdate<T,O = void> = (this: THookThisWithTransformAndPrevious<T, Collection<T>["update"]> & { previous: T, transform: (doc: T) => T }, userId: string|undefined, doc: T, fieldNames: string[], modifier: any, options: any) => O
+        type THookRemove<T,O = void> = (this: THookThisWithTransform<T, Collection<T>["remove"]>, userId: string|undefined, doc: T) => O
+        type THookUpsert<T,O = void> = (this: THookThis<T, Collection<T>["upsert"]>, userId: string|undefined, selector: any, modifier: any, options: any) => O
+        type THookBeforeFind<T,O = void> = (this: THookThis<T, Collection<T>["find"]>, userId: string|undefined, selector: any, options: any) => O
+        type THookAfterFind<T> = (this: THookThis<T, Collection<T>["find"]>, userId: string|undefined, selector: any, options: any, cursor: Cursor<T>) => void
+        type THookBeforeFindOne<T,O = void> = (this: THookThis<T, Collection<T>["findOne"]>, userId: string|undefined, selector: any, options: any) => O
+        type THookAfterFindOne<T> = (this: THookThis<T, Collection<T>["findOne"]>, userId: string|undefined, selector: any, options: any, doc: T) => void
         type THandler<F> = {remove(): void, replace(callback: F, options: any): void}
 
         interface Collection<T, U = T> {
@@ -41,20 +62,19 @@ declare module 'meteor/mongo' {
             before: {
                 insert<Fn = THookBeforeInsert<T,void|false>>(fn: Fn): THandler<Fn>
                 update<Fn = THookBeforeUpdate<T,void|false>>(fn: Fn): THandler<Fn>
-                remove<Fn = THookBeforeRemove<T,void|false>>(fn: Fn): THandler<Fn>
-                upsert<Fn = THookBeforeUpsert<T,void|false>>(fn: Fn): THandler<Fn>
+                remove<Fn = THookRemove<T,void|false>>(fn: Fn): THandler<Fn>
+                upsert<Fn = THookUpsert<T,void|false>>(fn: Fn): THandler<Fn>
                 find<Fn = THookBeforeFind<T,void|false>>(fn: Fn): THandler<Fn>
                 findOne<Fn = THookBeforeFindOne<T,void|false>>(fn: Fn): THandler<Fn>
             }
             after: {
-                insert<Fn = THookBeforeInsert<T>>(fn: Fn): THandler<Fn>
-                update<Fn = THookBeforeUpdate<T>>(fn: Fn): THandler<Fn>
-                remove<Fn = THookBeforeRemove<T>>(fn: Fn): THandler<Fn>
-                upsert<Fn = THookBeforeUpsert<T>>(fn: Fn): THandler<Fn>
+                insert<Fn = THookAfterInsert<T>>(fn: Fn): THandler<Fn>
+                update<Fn = THookAfterUpdate<T>>(fn: Fn, options?: { fetchPrevious?: boolean }): THandler<Fn>
+                remove<Fn = THookRemove<T>>(fn: Fn): THandler<Fn>
+                upsert<Fn = THookUpsert<T>>(fn: Fn): THandler<Fn>
                 find<Fn = THookAfterFind<T>>(fn: Fn): THandler<Fn>
                 findOne<Fn = THookAfterFindOne<T>>(fn: Fn): THandler<Fn>
             }
         }
     }
 }
-
