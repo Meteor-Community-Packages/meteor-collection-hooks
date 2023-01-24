f2c4afcd12751b887af33855411e5f8766e410e5
Koen [XII] <k.lavrijssen@gmail.com>
2023-01-24 21:44:42 +0100

    Update collection-hooks.js

--- collection-hooks.js
+++ collection-hooks.js
@@ -140,13 +140,13 @@ CollectionHooks.initOptions = (options, pointcut, method) =>
 CollectionHooks.extendOptions = (source, options, pointcut, method) =>
   ({ ...options, ...source.all.all, ...source[pointcut].all, ...source.all[method], ...source[pointcut][method] })
 
-CollectionHooks.getDocs = function getDocs (collection, selector, options, fetchFields) {
-  const findOptions = { transform: null, reactive: false };
+CollectionHooks.getDocs = function getDocs (collection, selector, options, fetchFields = {}) {
+  const findOptions = { transform: null, reactive: false }
 
   if (Object.keys(fetchFields).length > 0) {
-    findOptions.fields = fetchFields;
+    findOptions.fields = fetchFields
   }
-  
+
   /*
   // No "fetch" support at this time.
   if (!this._validators.fetchAllFields) {
