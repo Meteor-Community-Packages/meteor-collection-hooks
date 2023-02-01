6c794af71c3ac673f2e5497bc3332b3c08d3860f
jdgjsag67251 <88368191+jdgjsag67251@users.noreply.github.com>
2023-02-01 11:22:35 +0100

    296: Renamed test & added insert test

--- tests/find_after_hooks.js
+++ tests/find_after_hooks.js
@@ -11,18 +11,44 @@ Tinytest.addAsync('issue #296 - after update hook always finds all updated', fun
   })
 
   let beforeCalled = false
-  collection.before.update((userId, doc) => {
+  collection.before.update(() => {
     beforeCalled = true
   })
 
   let afterCalled = false
-  collection.after.update((userId, doc) => {
+  collection.after.update(() => {
     afterCalled = true
   })
 
   const id = collection.insert({ test: true })
 
   collection.update(id, { $set: { removedAt: new Date() } }, () => {
+    test.equal(beforeCalled, true)
+    test.equal(afterCalled, true)
+    next()
+  })
+})
+
+Tinytest.addAsync('issue #296 - after insert hook always finds all inserted', function (test, next) {
+  const collection = new Mongo.Collection(null)
+
+  collection.before.find((userId, selector) => {
+    selector.removedAt = { $exists: false }
+
+    return true
+  })
+
+  let beforeCalled = false
+  collection.before.insert(() => {
+    beforeCalled = true
+  })
+
+  let afterCalled = false
+  collection.after.insert(() => {
+    afterCalled = true
+  })
+
+  collection.insert({ removedAt: new Date() }, () => {
     test.equal(beforeCalled, true)
     test.equal(afterCalled, true)
     next()
