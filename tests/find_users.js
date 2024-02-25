22b42102708233bdcaaf883375791531a0491169
Bero <bero@stillwater.dev>
2024-02-25 12:46:58 +0100

    find_one fix

--- tests/find_users.js
+++ tests/find_users.js
@@ -5,7 +5,7 @@ import { InsecureLogin } from './insecure_login'
 // TODO(v3): both not working on client. selector is just { test: 1 } instead of { test: 1, a: 1, b: 1 }
 // When running in isolation, both tests pass
 // When running only one, both work, too
-Tinytest.addAsync('users - find hooks should be capable of being used on special Meteor.users collection', function (test, next) {
+Tinytest.addAsync('users - find hooks should be capable of being used on special Meteor.users collection', async function (test) {
   // eslint-disable-next-line array-callback-return
   const aspect1 = Meteor.users.before.find(function (userId, selector, options) {
     if (selector && selector.test) {
@@ -20,7 +20,7 @@ Tinytest.addAsync('users - find hooks should be capable of being used on special
     }
   })
 
-  InsecureLogin.ready(async function () {
+  await InsecureLogin.ready(async function () {
     const selector = { test: 1 }
     Meteor.users.find(selector)
     test.equal(Object.prototype.hasOwnProperty.call(selector, 'a'), true)
@@ -29,7 +29,6 @@ Tinytest.addAsync('users - find hooks should be capable of being used on special
     aspect2.remove()
 
     test.notEqual(await Meteor.users.find().countAsync(), 0)
-    next()
   })
 })
 
