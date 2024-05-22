df6988ad5a31634dc73f2036bba31cc8d63da435
Bero <bero@stillwater.dev>
2024-05-22 09:34:02 +0200

    updated eslint to v8, fixing top-level await error

--- tests/server/insecure_login.js
+++ tests/server/insecure_login.js
@@ -1,8 +1,8 @@
-import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
-import { InsecureLogin } from '../insecure_login';
+import { Meteor } from 'meteor/meteor'
+import { Accounts } from 'meteor/accounts-base'
+import { InsecureLogin } from '../insecure_login'
 
-InsecureLogin.run();
+InsecureLogin.run()
 
 // Meteor.users.remove({'username': 'InsecureLogin'})
 if (!(await Meteor.users.find({ username: 'InsecureLogin' }).countAsync())) {
@@ -10,17 +10,17 @@ if (!(await Meteor.users.find({ username: 'InsecureLogin' }).countAsync())) {
     username: 'InsecureLogin',
     email: 'test@test.com',
     password: 'password',
-    profile: { name: 'InsecureLogin' },
-  });
+    profile: { name: 'InsecureLogin' }
+  })
 }
 
 Accounts.registerLoginHandler(async function (options) {
-  if (!options.username) return;
-  const user = await Meteor.users.findOneAsync({ username: options.username });
-  if (!user) return;
+  if (!options.username) return
+  const user = await Meteor.users.findOneAsync({ username: options.username })
+  if (!user) return
   return {
-    userId: user._id,
-  };
-});
+    userId: user._id
+  }
+})
 
-export { InsecureLogin };
+export { InsecureLogin }
