c9de6e40b87df24bbc6866ee20358f39481036c8
Jan Dvorak <storytellercz@proton.me>
2024-05-26 11:57:23 +0200

    Bump to v2.0.0-beta.1

--- package.js
+++ package.js
@@ -3,12 +3,12 @@
 Package.describe({
   name: 'matb33:collection-hooks',
   summary: 'Extends Mongo.Collection with before/after hooks for insert/update/upsert/remove/find/findOne',
-  version: '1.4.0-beta.1',
+  version: '2.0.0-beta.1',
   git: 'https://github.com/Meteor-Community-Packages/meteor-collection-hooks'
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom(['2.8.1', '2.12', '3.0-rc.0'])
+  api.versionsFrom(['3.0-rc.2'])
 
   api.use([
     'mongo',
@@ -18,7 +18,7 @@ Package.onUse(function (api) {
     'ecmascript'
   ])
 
-  api.use('zodern:types@1.0.11', 'server')
+  api.use('zodern:types@1.0.13', 'server')
 
   api.use(['accounts-base'], ['client', 'server'], { weak: true })
 
@@ -31,7 +31,7 @@ Package.onUse(function (api) {
 Package.onTest(function (api) {
   // var isTravisCI = process && process.env && process.env.TRAVIS
 
-  api.versionsFrom(['2.12', '3.0-rc.0'])
+  api.versionsFrom(['3.0-rc.2'])
 
   api.use([
     'matb33:collection-hooks',
