2d09e8b17392c06af1fe399b071dd1948a7b37c9
harryadel <harryadelb@gmail.com>
2025-07-11 16:58:13 +0300

    Re-add old insert_both.test.js

--- tests-app/insert_both.test.js
+++ tests-app/insert_both.test.js
@@ -2,25 +2,130 @@ import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
 import expect from 'expect'
 
-describe('Insert Both Tests', function () {
-  it('should fire before and after hooks on server and client for normal collection', async function () {
-    const isServer = Meteor.isServer
-    const collection = new Mongo.Collection(null)
-    let beforeUserId = 'not set'
-    let afterUserId = 'not set'
+if (Meteor.isServer) {
+  const collection1 = new Mongo.Collection('test_insert_collection1')
 
-    collection.before.insert(function (userId, doc) {
-      beforeUserId = userId
-      expect(isServer).toBe(Meteor.isServer)
+  describe('insert - server side', function () {
+    it('collection1 document should have extra property added to it before it is inserted', async function () {
+      const tmp = {}
+
+      await collection1.removeAsync({})
+
+      collection1.before.insert(async function (userId, doc) {
+        // There should be no userId because the insert was initiated
+        // on the server -- there's no correlation to any specific user
+        tmp.userId = userId // HACK: can't test here directly otherwise refreshing test stops execution here
+        doc.before_insert_value = true
+      })
+
+      await collection1.insertAsync({ start_value: true })
+
+      expect(
+        await collection1
+          .find({ start_value: true, before_insert_value: true })
+          .countAsync()
+      ).toBe(1)
+      expect(tmp.userId).toBe(undefined)
     })
-
-    collection.after.insert(function (userId, doc) {
-      afterUserId = userId
-      expect(isServer).toBe(Meteor.isServer)
-    })
-
-    await collection.insertAsync({ test: true })
-    expect(beforeUserId).toBe(undefined)
-    expect(afterUserId).toBe(undefined)
   })
-})
+}
+
+const collection2 = new Mongo.Collection('test_insert_collection2')
+
+if (Meteor.isServer) {
+  // full client-side access
+  collection2.allow({
+    insert () {
+      return true
+    },
+    insertAsync () {
+      return true
+    },
+    update () {
+      return true
+    },
+    remove () {
+      return true
+    }
+  })
+
+  Meteor.methods({
+    test_insert_reset_collection2: function () {
+      return collection2.removeAsync({})
+    }
+  })
+
+  Meteor.publish('test_insert_publish_collection2', function () {
+    return collection2.find()
+  })
+
+  collection2.before.insert(function (userId, doc) {
+    // console.log('test_insert_collection2 BEFORE INSERT', userId, doc)
+    doc.server_value = true
+  })
+}
+
+if (Meteor.isClient) {
+  // Mock getUserId to return a fake userId for client-side hooks
+  const originalGetUserId = CollectionHooks.getUserId
+  CollectionHooks.getUserId = () => 'mock-user-id'
+
+  Meteor.subscribe('test_insert_publish_collection2')
+
+  describe('insert - client side', function () {
+    it('collection2 document on client should have client-added and server-added extra properties added to it before it is inserted', async function () {
+      collection2.before.insert(function (userId, doc) {
+        // console.log('test_insert_collection2 BEFORE INSERT', userId, doc)
+        expect(userId).not.toBe(undefined)
+        expect(userId).toBe('mock-user-id') // Verify our mock is working
+        expect(collection2.find({ start_value: true }).count()).toBe(0)
+        doc.client_value = true
+      })
+
+      collection2.after.insert(function (userId, doc) {
+        // console.log('test_insert_collection2 AFTER INSERT', userId, doc)
+        expect(this._id).not.toBe(undefined)
+      })
+
+      await Meteor.callAsync('test_insert_reset_collection2')
+      // console.log('test_insert_collection2 INSERT')
+      await collection2.insertAsync({ start_value: true })
+
+      expect(
+        collection2
+          .find({
+            start_value: true,
+            client_value: true,
+            server_value: true
+          })
+          .count()
+      ).toBe(1)
+    })
+  })
+
+  describe('insert - sync methods', function () {
+    it('hooks are not called for sync methods', function () {
+      const collectionForSync = new Mongo.Collection(null)
+      let beforeCalled = false
+      let afterCalled = false
+      
+      collectionForSync.before.insert(function (userId, selector, options) {
+        beforeCalled = true
+      })
+      
+      collectionForSync.after.insert(function (userId, selector, options) {
+        afterCalled = true
+      })
+
+      const res = collectionForSync.insert({ test: 1 })
+      expect(typeof res).toBe('string')
+      expect(beforeCalled).toBe(false)
+      expect(afterCalled).toBe(false)
+    })
+  })
+
+  // Clean up mock after client tests
+  after(function () {
+    CollectionHooks.getUserId = originalGetUserId
+  })
+}
