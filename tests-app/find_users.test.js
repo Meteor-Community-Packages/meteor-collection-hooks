9f61cfb3f05f33a82fb56b49aaa67965472f3208
harryadel <harryadelb@gmail.com>
2025-07-17 16:01:34 +0300

    Fix find and findOne tests

--- tests-app/find_users.test.js
+++ tests-app/find_users.test.js
@@ -6,40 +6,29 @@ import expect from 'expect'
 // When running in isolation, both tests pass
 // When running only one, both work, too
 describe('users - find hooks', function () {
-  it('should be capable of being used on special Meteor.users collection', async function () {
-    const originalGetUserId = CollectionHooks.getUserId
-    CollectionHooks.getUserId = () => 'mock-user-id'
+  it('should be capable of being used on special Meteor.users collection', async function () {      
 
-    let beforeCalled = false
-    let afterCalled = false
+    const aspect1 = Meteor.users.before.find(function (userId, selector, options) {
+      if (selector && selector.test) {
+        selector.a = 1
+      }
+    })
 
-    try {      
-      const aspect1 = Meteor.users.before.find(function (userId, selector, options) {
-        beforeCalled = true
-        if (selector && selector.test) {
-          selector.a = 1
-        }
-      })
+    const aspect2 = Meteor.users.after.find(function (userId, selector, options) {
+      if (selector && selector.test) {
+        selector.b = 1
+      }
+    })
 
-      const aspect2 = Meteor.users.after.find(function (userId, selector, options) {
-        afterCalled = true
-        if (selector && selector.test) {
-          selector.b = 1
-        }
-      })
+    const selector = { test: 1 }
+    
+    const cursor = Meteor.users.find(selector)
+    
+    expect(Object.prototype.hasOwnProperty.call(selector, 'a')).toBe(true)
+    expect(Object.prototype.hasOwnProperty.call(selector, 'b')).toBe(true)
+    aspect1.remove()
+    aspect2.remove()
 
-      const selector = { test: 1 }
-      
-      const cursor = Meteor.users.find(selector)
-      
-      expect(Object.prototype.hasOwnProperty.call(selector, 'a')).toBe(true)
-      expect(Object.prototype.hasOwnProperty.call(selector, 'b')).toBe(true)
-      aspect1.remove()
-      aspect2.remove()
-
-    } finally {
-      CollectionHooks.getUserId = originalGetUserId
-    }
   })
 
   it('should be capable of being used on wrapped Meteor.users collection', async function () {
@@ -79,6 +68,5 @@ describe('users - find hooks', function () {
     expect(await Meteor.users.find().countAsync()).not.toBe(0)
 
     Meteor.users.find = MeteorUsersFind
-    
   })
 })
